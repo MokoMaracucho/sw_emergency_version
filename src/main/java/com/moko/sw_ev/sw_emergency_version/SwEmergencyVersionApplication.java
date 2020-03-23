1208e6e0158c683c71538f2ddaa8f09829fd5592
MokoMaracucho <desmoulin.federico@gmail.com>
2020-03-23 11:18:16 +0000

    Base Spring : Spring Web + Thymleaf + Spring Security

---
+++ src/main/java/com/moko/sw_ev/sw_emergency_version/SwEmergencyVersionApplication.java
@@ -0,0 +1,13 @@
+package com.moko.sw_ev.sw_emergency_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwEmergencyVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwEmergencyVersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moko/sw_ev/sw_emergency_version/SwEmergencyVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moko.sw_ev.sw_emergency_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwEmergencyVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
