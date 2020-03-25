1e04522a81f74b7f5e5b4613171bd2bfc0ec5872
MokoMaracucho <desmoulin.federico@gmail.com>
2020-03-25 19:18:16 +0000

    DAO : UserDao.java

---
+++ src/main/java/com/moko/sw_ev/sw_emergency_version/dao/userDao.java
@@ -0,0 +1,15 @@
+package com.moko.sw_ev.sw_emergency_version.dao;
+
+import java.util.UUID;
+
+import com.moko.sw_ev.sw_emergency_version.model.User;
+
+public interface userDao {
+
+	int insertUser(UUID idUser, User user);
+	
+	default int addUtilisateur(User user) {
+		UUID idUser = UUID.randomUUID();
+		return insertUser(idUser, user);
+	}
+}
