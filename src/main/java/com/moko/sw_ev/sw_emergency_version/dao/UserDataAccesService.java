a6d37d7ec416fd9c64198d7e4d3ae356295fc38f
MokoMaracucho <desmoulin.federico@gmail.com>
2020-03-26 23:09:56 +0000

    DAO : Enum

---
+++ src/main/java/com/moko/sw_ev/sw_emergency_version/dao/UserDataAccesService.java
@@ -0,0 +1,56 @@
+package com.moko.sw_ev.sw_emergency_version.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.moko.sw_ev.sw_emergency_version.model.Gender;
+import com.moko.sw_ev.sw_emergency_version.model.Privilege;
+import com.moko.sw_ev.sw_emergency_version.model.User;
+
+public class UserDataAccesService implements UserDao {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public UserDataAccesService(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public int insertUser(UUID idUser, User user) {
+		return 0;
+	}
+
+	@Override
+	public List<User> selectAllUsers() {
+		final String sql = "SELECT id_user, prenom_user FROM tb_user";
+		return jdbcTemplate.query(sql, (resultSet, i) -> {
+			UUID id_user = UUID.fromString(resultSet.getString("id_user"));
+			Gender gender_user = (Gender) resultSet.getObject("gender_user");
+			String first_name_user = resultSet.getString("first_name_user");
+			String last_name_user = resultSet.getString("last_name_user");
+			int age_user = resultSet.getInt("age_user");
+			String email_user = resultSet.getString("email_user");
+			String occupation_user = resultSet.getString("occupation_user");
+			Privilege privilege_user = (Privilege) resultSet.getObject("privilege_user");
+			return new User(id_user, gender_user, first_name_user, last_name_user, age_user, email_user, occupation_user, privilege_user);
+		});
+	}
+
+	@Override
+	public Optional<User> selectUserById(UUID idUser) {
+		return null;
+	}
+
+	@Override
+	public boolean deleteUserById(UUID idUser) {
+		return false;
+	}
+
+	@Override
+	public boolean updateUserById(UUID idUser, User user) {
+		return false;
+	}
+}
--- src/main/resources/db/migration/V1__User.sql
+++ src/main/resources/db/migration/V1__User.sql
@@ -1,11 +1,14 @@
+CREATE TYPE enum_gender AS ENUM ('FEMALE', 'MALE');
+CREATE TYPE enum_privilege AS ENUM ('USER', 'ADMINISTRATEUR', 'SUPER_ADMINISTRATEUR');
+
 CREATE TABLE tb_user (
     id_user UUID PRIMARY KEY,
-    gender_user VARCHAR(6) NOT NULL,
+    gender_user enum_gender NOT NULL,
     first_name_user VARCHAR(30) NOT NULL,
     last_name_user VARCHAR(30) NOT NULL,
     age_user INTEGER NOT NULL,
     email_user VARCHAR(50) NOT NULL,
     password_user VARCHAR(60) NOT NULL,
     occupation_user VARCHAR(50) NOT NULL,
-    privilege_user VARCHAR(20)
+    privilege_user enum_privilege NOT NULL
 );
