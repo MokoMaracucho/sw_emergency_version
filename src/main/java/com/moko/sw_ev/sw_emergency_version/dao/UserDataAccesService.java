9fda4bc43dd486e5d2a92640da5443bce6cde05b
MokoMaracucho <desmoulin.federico@gmail.com>
2020-03-26 23:32:52 +0000

    UserDataAccessService : selectUserById()

--- src/main/java/com/moko/sw_ev/sw_emergency_version/dao/UserDataAccesService.java
+++ src/main/java/com/moko/sw_ev/sw_emergency_version/dao/UserDataAccesService.java
@@ -25,7 +25,7 @@ public class UserDataAccesService implements UserDao {
 
 	@Override
 	public List<User> selectAllUsers() {
-		final String sql = "SELECT id_user, prenom_user FROM tb_user";
+		final String sql = "SELECT id_user, first_name_user FROM tb_user";
 		return jdbcTemplate.query(sql, (resultSet, i) -> {
 			UUID id_user = UUID.fromString(resultSet.getString("id_user"));
 			Gender gender_user = (Gender) resultSet.getObject("gender_user");
@@ -35,13 +35,47 @@ public class UserDataAccesService implements UserDao {
 			String email_user = resultSet.getString("email_user");
 			String occupation_user = resultSet.getString("occupation_user");
 			Privilege privilege_user = (Privilege) resultSet.getObject("privilege_user");
-			return new User(id_user, gender_user, first_name_user, last_name_user, age_user, email_user, occupation_user, privilege_user);
+			return new User(
+				id_user, 
+				gender_user, 
+				first_name_user, 
+				last_name_user, 
+				age_user, 
+				email_user, 
+				occupation_user, 
+				privilege_user
+			);
 		});
 	}
 
 	@Override
-	public Optional<User> selectUserById(UUID idUser) {
-		return null;
+	public Optional<User> selectUserById(UUID id) {
+		final String sql = "SELECT id_user, first_name_user FROM tb_user";
+		User user = jdbcTemplate.queryForObject(
+				sql, 
+				new Object[] {id}, 
+				(resultSet, i) -> {
+					UUID id_user = UUID.fromString(resultSet.getString("id_user"));
+					Gender gender_user = (Gender) resultSet.getObject("gender_user");
+					String first_name_user = resultSet.getString("first_name_user");
+					String last_name_user = resultSet.getString("last_name_user");
+					int age_user = resultSet.getInt("age_user");
+					String email_user = resultSet.getString("email_user");
+					String occupation_user = resultSet.getString("occupation_user");
+					Privilege privilege_user = (Privilege) resultSet.getObject("privilege_user");
+					return new User(
+						id_user, 
+						gender_user, 
+						first_name_user, 
+						last_name_user, 
+						age_user, 
+						email_user, 
+						occupation_user, 
+						privilege_user
+					);
+				}
+			);
+		return Optional.ofNullable(user);
 	}
 
 	@Override
